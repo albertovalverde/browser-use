5160ebdc7f3f03a70302e5a3e9026b032764530d
Alberto Valverde <albertovalverd@hotmail.com>
2025-01-12 19:13:33 +0100

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ async def main():
     agent = Agent(
         task="""Go to the Boletín Oficial del Estado website, 
         search for 'despidos colectivos' in the search bar, 
-        return the last 5 titles listed""",
+        return the last 5 titles displayed""",
         llm=ChatOpenAI(model="gpt-4o",temperature=0.50)
     )
     result = await agent.run()
